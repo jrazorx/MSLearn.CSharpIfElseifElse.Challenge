7d74f381b0cd79df0a37be9809135df701d52cc4
jra <julien.gault.50@gmail.com>
2024-02-19 16:38:37 +0100

    added messages for renewal

--- MSLearn.CSharpIfElseifElse.Challenge/Program.cs
+++ MSLearn.CSharpIfElseifElse.Challenge/Program.cs
@@ -2,4 +2,21 @@
 int daysUntilExpiration = random.Next(12);
 int discountPercentage = 0;
 
-// Your code goes here
+Console.WriteLine($"Days until expiration: {daysUntilExpiration}");
+
+if (daysUntilExpiration < 1)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expires within a day!");
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
